9b0efc858a6ecb61962f20dba942cca10215e82f
Kenichi Sato <ksato9700@gmail.com>
2020-10-27 16:06:19 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use bitcoin::blockdata::block::Block;
+use bitcoin::blockdata::transaction::Transaction;
+use bitcoin::consensus::encode::deserialize;
+use std::convert::TryInto;
+
+fn main() {
+    let bitcoind_zmq_url = "tcp://btc-node-testnet-01.do.fressets.net:28332";
+
+    let ctx = zmq::Context::new();
+    let socket = ctx.socket(zmq::SUB).unwrap();
+    socket.set_subscribe(b"hashblock").unwrap();
+    socket.set_subscribe(b"hashtx").unwrap();
+    socket.set_subscribe(b"rawblock").unwrap();
+    socket.set_subscribe(b"rawtx").unwrap();
+    socket.set_subscribe(b"sequence").unwrap();
+    socket.connect(bitcoind_zmq_url).expect("failed to connect");
+
+    loop {
+        let data = socket.recv_multipart(0).unwrap();
+        let topic = String::from_utf8(data.get(0).unwrap().to_vec()).unwrap();
+
+        let sequence_slice: Box<[u8]> = data.get(2).unwrap().to_vec().into_boxed_slice();
+        let sequence_array: Box<[u8; 4]> = sequence_slice.try_into().unwrap();
+        let sequence = u32::from_le_bytes(*sequence_array);
+        println!("topic: {:?}", topic);
+        println!("sequence: {:?}", sequence);
+
+        let body = data.get(1).unwrap();
+        if topic == "rawtx" {
+            let tx: Transaction = deserialize(&body).unwrap();
+            println!("tx: {:?}", tx);
+        } else if topic == "rawblock" {
+            let block: Block = deserialize(&body).unwrap();
+            println!("block: {:?}", block);
+        }
+    }
+}
